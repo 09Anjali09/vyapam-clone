fc31ae7dfd863d4af70942d5dbb859c29b38228c
Anjali <09Anjali09@gmail.com>
2024-02-05 13:13:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function headimgbtn1(){
+    let headbt=document.getElementById("headimage");
+    headbt.src="headim2.jpg";
+}
+
+function headimgbtn2(){
+    let headbt=document.getElementById("headimage");
+    headbt.src="headImage.jpg";
+}
+
+function headimgbtn3(){
+    let headbt=document.getElementById("headimage");
+    headbt.src="headimg3.jpeg";
+}
